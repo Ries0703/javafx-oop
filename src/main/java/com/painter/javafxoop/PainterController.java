19bd9c1cab2b72f6967887891a8394dfb2a7c072
Ries0703 <117291686+Ries0703@users.noreply.github.com>
2024-06-01 19:27:58 +0700

    finish eraser for Painter

--- src/main/java/com/painter/javafxoop/PainterController.java
+++ src/main/java/com/painter/javafxoop/PainterController.java
@@ -1,18 +1,34 @@
 package com.painter.javafxoop;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-
 import javafx.event.ActionEvent;
-
 
 public class PainterController {
 
     @FXML
-    private Pane drawingAreaPane = new Pane();
+    private Pane drawingAreaPane;
+
+    @FXML
+    private RadioButton penRadioButton;
+
+    @FXML
+    private RadioButton eraserRadioButton;
+
+    private ToggleGroup toolsToggleGroup;
+
+    @FXML
+    public void initialize() {
+        toolsToggleGroup = new ToggleGroup();
+        penRadioButton.setToggleGroup(toolsToggleGroup);
+        eraserRadioButton.setToggleGroup(toolsToggleGroup);
+        penRadioButton.setSelected(true);
+    }
 
     @FXML
     public void clearButtonPressed(ActionEvent actionEvent) {
@@ -21,7 +37,8 @@ public class PainterController {
 
     @FXML
     public void drawingAreaMouseDragged(MouseEvent mouseEvent) {
-        Circle circle = new Circle(mouseEvent.getX(), mouseEvent.getY(), 4, Color.BLACK);
+        Color color = penRadioButton.isSelected() ? Color.BLACK : Color.WHITE;
+        Circle circle = new Circle(mouseEvent.getX(), mouseEvent.getY(), 4, color);
         drawingAreaPane.getChildren().add(circle);
     }
 }
